aae50d0aa878668f70febbd9c7a77110b28d1cf3
JeffWu0624 <jeffwu0624@hotmail.com>
2021-03-31 00:22:51 +0800

    finish PostOfficeTest

--- WebApplication1/WebApplication1.UnitTests/PostOfficeTests.cs
+++ WebApplication1/WebApplication1.UnitTests/PostOfficeTests.cs
@@ -31,9 +31,40 @@ namespace WebApplication1.UnitTests
         }
 
         [Test]
-        public void GetFee_ShouldBe_When()
+        public void GetFee_ShouldBe_WhenWeightMoreThenSize()
         {
-            
+            _postOffice.ShipProduct = new Product()
+            {
+                Weight = 10,
+                Length = 10,
+                Width = 10,
+                Height = 10
+            };
+
+            FeeShouldBe(38);
+        }
+
+        [Test]
+        public void GetFee_ShouldBe_WhenWeightLessThenSize()
+        {
+            _postOffice.ShipProduct = new Product()
+            {
+                Weight = 10,
+                Length = 1000,
+                Width = 1000,
+                Height = 1000
+            };
+
+            FeeShouldBe(180);
+        }
+
+        private void FeeShouldBe(int expected)
+        {
+            _postOffice.Calculated();
+
+            var fee = _postOffice.GetFee();
+
+            Assert.That(fee, Is.EqualTo(expected).Within(0.9));
         }
     }
 }
