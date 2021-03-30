fec3906f7dc3583cf88910c4b7609a7a6d63535f
JeffWu0624 <jeffwu0624@hotmail.com>
2021-03-30 15:28:18 +0800

    extract method

--- WebApplication1/WebApplication1/Index.aspx.cs
+++ WebApplication1/WebApplication1/Index.aspx.cs
@@ -15,61 +15,76 @@ namespace WebApplication1
             {
                 if ("1".Equals(ddlLogistics.SelectedValue))
                 {
-                    ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
-
-                    var weight = Convert.ToDouble(txtWeight.Text);
-
-                    if (weight > 20)
-                    {
-                        ltrFee.Text = 500.ToString("C");
-                    }
-                    else
-                    {
-                        ltrFee.Text = (100 + weight * 10).ToString("C");
-                    }
+                    CalculatedByBlackCat();
                 }
                 else if ("2".Equals(ddlLogistics.SelectedValue))
                 {
-                    ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
-
-                    var width = Convert.ToDouble(txtWidth.Text);
-                    var length = Convert.ToDouble(txtLength.Text);
-                    var height = Convert.ToDouble(txtHeight.Text);
-
-                    var size = (width * length * height);
-
-                    if (length > 100 || width > 100 || height > 100)
-                    {
-                        ltrFee.Text = (size * 0.0000353 * 1100 + 500).ToString("C");
-                    }
-                    else
-                    {
-                        ltrFee.Text = (size * 0.0000353 * 1200).ToString("C");
-                    }
+                    CalculatedByHsinchu();
                 }
                 else if ("3".Equals(ddlLogistics.SelectedValue))
                 {
-                    ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
-
-                    var weight = Convert.ToDouble(txtWeight.Text);
-                    var feeByWeight = 80 + (weight * 10);
-
-                    var width = Convert.ToDouble(txtWidth.Text);
-                    var length = Convert.ToDouble(txtLength.Text);
-                    var height = Convert.ToDouble(txtHeight.Text);
-
-                    var size = width * length * height;
-                    var feeBySize = size * 0.0000353 * 1100;
-
-                    if (feeByWeight < feeBySize)
-                    {
-                        ltrFee.Text = feeByWeight.ToString("C");
-                    }
-                    else
-                    {
-                        ltrFee.Text = feeBySize.ToString("C");
-                    }
+                    CalculatedByPostOffice();
                 }
+            }
+        }
+
+        private void CalculatedByPostOffice()
+        {
+            ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
+
+            var weight = Convert.ToDouble(txtWeight.Text);
+            var feeByWeight = 80 + (weight * 10);
+
+            var width = Convert.ToDouble(txtWidth.Text);
+            var length = Convert.ToDouble(txtLength.Text);
+            var height = Convert.ToDouble(txtHeight.Text);
+
+            var size = width * length * height;
+            var feeBySize = size * 0.0000353 * 1100;
+
+            if (feeByWeight < feeBySize)
+            {
+                ltrFee.Text = feeByWeight.ToString("C");
+            }
+            else
+            {
+                ltrFee.Text = feeBySize.ToString("C");
+            }
+        }
+
+        private void CalculatedByHsinchu()
+        {
+            ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
+
+            var width = Convert.ToDouble(txtWidth.Text);
+            var length = Convert.ToDouble(txtLength.Text);
+            var height = Convert.ToDouble(txtHeight.Text);
+
+            var size = (width * length * height);
+
+            if (length > 100 || width > 100 || height > 100)
+            {
+                ltrFee.Text = (size * 0.0000353 * 1100 + 500).ToString("C");
+            }
+            else
+            {
+                ltrFee.Text = (size * 0.0000353 * 1200).ToString("C");
+            }
+        }
+
+        private void CalculatedByBlackCat()
+        {
+            ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
+
+            var weight = Convert.ToDouble(txtWeight.Text);
+
+            if (weight > 20)
+            {
+                ltrFee.Text = 500.ToString("C");
+            }
+            else
+            {
+                ltrFee.Text = (100 + weight * 10).ToString("C");
             }
         }
     }
