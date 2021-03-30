f49ae65c2bff2c2934a9bba7629b58dc55fb7190
JeffWu0624 <jeffwu0624@hotmail.com>
2021-03-30 11:45:49 +0800

    cretae web project

---
+++ WebApplication1/WebApplication1/Index.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace WebApplication1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculator_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                if ("1".Equals(ddlLogistics.SelectedValue))
+                {
+                    ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
+
+                    var weight = Convert.ToDouble(txtWeight.Text);
+
+                    if (weight > 20)
+                    {
+                        ltrFee.Text = 500.ToString("C");
+                    }
+                    else
+                    {
+                        ltrFee.Text = (100 + weight * 10).ToString("C");
+                    }
+                }
+                else if ("2".Equals(ddlLogistics.SelectedValue))
+                {
+                    ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
+
+                    var width = Convert.ToDouble(txtWidth.Text);
+                    var length = Convert.ToDouble(txtLength.Text);
+                    var height = Convert.ToDouble(txtHeight.Text);
+
+                    var size = (width * length * height);
+
+                    if (length > 100 || width > 100 || height > 100)
+                    {
+                        ltrFee.Text = (size * 0.0000353 * 1100 + 500).ToString("C");
+                    }
+                    else
+                    {
+                        ltrFee.Text = (size * 0.0000353 * 1200).ToString("C");
+                    }
+                }
+                else if ("3".Equals(ddlLogistics.SelectedValue))
+                {
+                    ltrLogistics.Text = ddlLogistics.SelectedItem.Text;
+
+                    var weight = Convert.ToDouble(txtWeight.Text);
+                    var feeByWeight = 80 + (weight * 10);
+
+                    var width = Convert.ToDouble(txtWidth.Text);
+                    var length = Convert.ToDouble(txtLength.Text);
+                    var height = Convert.ToDouble(txtHeight.Text);
+
+                    var size = width * length * height;
+                    var feeBySize = size * 0.0000353 * 1100;
+
+                    if (feeByWeight < feeBySize)
+                    {
+                        ltrFee.Text = feeByWeight.ToString("C");
+                    }
+                    else
+                    {
+                        ltrFee.Text = feeBySize.ToString("C");
+                    }
+                }
+            }
+        }
+    }
+}
